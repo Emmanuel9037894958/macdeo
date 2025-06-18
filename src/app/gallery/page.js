5dc82e82a3ad12872205cf260b82fec9a54258bf
Emmanuel9037894958 <emmanuelamamchukwu@gmail.com>
2025-06-18 10:11:45 +0100

    apple

--- src/app/gallery/page.js
+++ src/app/gallery/page.js
@@ -42,7 +42,7 @@ function Gallery() {
             type="button"
             className="relative inline-flex items-center justify-center p-0.5 overflow-hidden text-sm font-medium text-white rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 hover:text-white focus:ring-4 focus:outline-none focus:ring-green-200 w-62 mt-20"
           >
-            <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-transparent font-bold text-lg">
+            <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-black dark:bg-gray-900 rounded-md group-hover:bg-transparent font-bold text-lg">
               GO BACK!
             </span>
           </button>
